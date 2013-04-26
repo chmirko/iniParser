2b191b0b91787a955a67d43989b6626ffcb1ebc1
m9ra <m9racent@centrum.cz>
2013-04-26 19:27:57 +0200

    Range attribute

---
+++ ConfigReader/ConfigReader/RangeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ConfigReader
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class RangeAttribute:Attribute
+    {
+        public object UpperBound { get; set; }
+        public object LowerBound { get; set; }
+    }
+}
