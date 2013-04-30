91fc49ca7c163e4f0d4cd663b83f58d2a88530b1
m9ra <m9racent@centrum.cz>
2013-04-30 16:43:11 +0200

    EnumConverter.EnumType added

---
+++ ConfigReader/ConfigReader/Parsing/Converters/EnumConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConfigReader.Parsing.Converters
+{
+    class EnumConverter:IValueConverter
+    {
+        /// <summary>
+        /// Type of enum which can be converted by this converter.
+        /// </summary>
+        internal readonly Type EnumType;
+
+        private EnumConverter(OptionInfo optionInfo)
+        {
+        }
+
+        /// <summary>
+        /// Create enum converter for optionInfo. 
+        /// NOTE: Enum can be created according to ElementType and not ExpectedType.
+        /// </summary>
+        /// <param name="optionInfo">Option which converter will be created.</param>
+        /// <param name="converter">Result converter if it was created, null otherwise.</param>
+        /// <returns>True if converter was successfuly created, false otherwise.</returns>
+        internal static bool TryCreate(OptionInfo optionInfo, out EnumConverter converter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Deserialize(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
